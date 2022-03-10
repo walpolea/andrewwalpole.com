2e288ed55b8e12e3db781b0516f3dab66867203e
Andrew Walpole <walpolea@gmail.com>
2022-03-09 20:47:39 -0800

    ditched-design

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginDate = require("eleventy-plugin-date");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginDate);
+  eleventyConfig.addPlugin(pluginRss);
+
   //watch and copy over the bundles css and js
   eleventyConfig.addPassthroughCopy({ "./src/_site/assets": "assets" });
   eleventyConfig.addWatchTarget("./src/_site/assets");
