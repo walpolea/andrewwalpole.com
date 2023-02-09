92ada5aa308db20099e77492899899e35599be08
Andrew Walpole <walpolea@gmail.com>
2023-02-09 06:23:15 -0800

    add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site:`https://andrewwalpole.com`,
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
 });
