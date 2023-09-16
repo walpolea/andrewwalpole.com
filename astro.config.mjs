0a155f6948ab62ae392fd8a633b5ef06a1c34efa
Andrew Walpole <walpolea@gmail.com>
2023-09-16 08:44:23 -0700

    add 3d printing work

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import sitemap from '@astrojs/sitemap';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  site:`https://andrewwalpole.com`,
+  site: `https://andrewwalpole.com`,
   integrations: [vue(), sitemap({
-    filter: (page) => !page.includes('/preview/')
-  })],
+    filter: page => !page.includes('/preview/')
+  }), mdx()]
 });
